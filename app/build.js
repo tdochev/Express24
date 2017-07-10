208c299262d99bdece1a12d8f7ab2a72b7cd626a
Tsvetomir <tdd@outlook.com>
2017-07-10 11:57:04 +0300

    added morgan for logging

--- app/build.js
+++ app/build.js
@@ -1,8 +1,25 @@
+/* globals __dirname */
+
 const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+const logDirectory = path.join(__dirname, 'log');
+
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+}
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: logDirectory,
+});
 
 const build = () => {
     const app = express();
     app.set('view engine', 'pug');
+    app.use(morgan('combined', { stream: accessLogStream }));
 
     return app;
 };
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/pug/index.d.ts" />
