787018354f25430e1e2bd8619000d7c37befbe58
Tsvetomir <tdd@outlook.com>
2017-07-21 12:49:06 +0300

    removed unnecessary typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/express-session/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/passport/index.d.ts" />
-/// <reference path="globals/pug/index.d.ts" />
