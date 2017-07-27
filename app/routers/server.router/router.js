85ef1546b85f4ae916453f9ec9f190e11fcc470f
Tsvetomir Dochev <tddochev@gmail.com>
2017-07-27 11:32:23 +0300

    added some pug views

--- app/routers/server.router/router.js
+++ app/routers/server.router/router.js
@@ -3,7 +3,10 @@ const attachTo = (app, data) => {
         res.send('<h1>Users server route. It works</h1>');
     });
     app.get('/books', (req, res) => {
-        res.send('<h1>Booksvserver route. It works</h1>');
+        res.send('<h1>Books server route. It works</h1>');
+    });
+    app.get('/authors', (req, res) => {
+        res.send('<h1>Authors route. It works</h1>');
     });
 };
 
