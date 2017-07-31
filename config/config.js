589e1eab622849b3915bb1b23bfdecd68dc2c9f9
Tsvetomir <tdd@outlook.com>
2017-07-31 23:03:44 +0300

    config fix

--- config/config.js
+++ config/config.js
@@ -3,8 +3,10 @@
 // eslint-disable-next-line no-process-env
 const port = process.env.PORT || '3001';
 const connectionString = 'mongodb://localhost/express-24';
+const sessionStoreConnectionString = 'mongodb: //localhost/session'
 
 module.exports = {
     port,
     connectionString,
+    sessionStoreConnectionString,
 };
